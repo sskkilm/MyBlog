015333244fb2b4ebf7807681241a52eb28aadd90
sskkilm <sskkilm@naver.com>
2024-08-04 01:54:09 +0900

    feat: update post implementation

--- src/main/java/com/sskkilm/myblog/entity/BaseEntity.java
+++ src/main/java/com/sskkilm/myblog/entity/BaseEntity.java
@@ -3,7 +3,8 @@ package com.sskkilm.myblog.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
--- src/main/java/com/sskkilm/myblog/service/PostService.java
+++ src/main/java/com/sskkilm/myblog/service/PostService.java
@@ -1,14 +1,20 @@
 package com.sskkilm.myblog.service;
 
 import com.sskkilm.myblog.dto.PostCreateDto;
+import com.sskkilm.myblog.dto.PostUpdateDto;
+import com.sskkilm.myblog.entity.Post;
 import com.sskkilm.myblog.entity.User;
 import com.sskkilm.myblog.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
+
+    public static final int MAXIMUM_EDITABLE_DATE = 10;
 
     private final PostRepository postRepository;
 
@@ -19,5 +25,17 @@ public class PostService {
                         PostCreateDto.Request.toEntity(user, request)
                 )
         );
+    }
+
+    public PostUpdateDto.Response updatePost(Long postId, PostUpdateDto.Request request) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("Post not found -> " + postId));
+        if (post.getCreatedAt().plusDays(MAXIMUM_EDITABLE_DATE).isBefore(LocalDateTime.now())) {
+            throw new RuntimeException("생성일 기준 10일 이후 수정불가");
+        }
+
+        post.update(request.getTitle(), request.getContent());
+
+        return PostUpdateDto.Response.fromEntity(post);
     }
 }
